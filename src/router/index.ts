9b16279fe286e5844a9748c061394dbde35d1d24
Furkan Şen <furkansen202@gmail.com>
2024-09-30 20:27:48 +0300

    [index.ts]:[created] The configuration settings for the Router component are complete and the relevant pages have loaded successfully.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,40 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
+import { createRouter, createWebHistory } from "vue-router";
+import { NavBar } from "@/constants/appConstants";
+import Default from "@/layouts/Default.vue";
+import Home from "@/pages/Home.vue";
+import Trending from "@/pages/Trending/Index.vue";
 
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+const routes = [
+  {
+    path: "/",
+    redirect: NavBar.HOME_VIEW.path,
+    component: Default,
+    children: [
+      {
+        path: NavBar.HOME_VIEW.path,
+        name: NavBar.HOME_VIEW.name,
+        component: Home,
+      },
+      /* {
+        path: NavBar.SEARCH_FOR_A_MOVIE.path,
+        name: NavBar.SEARCH_FOR_A_MOVIE.name,
+        component: About,
+      }, */
+      {
+        path: NavBar.TRENDS.path,
+        name: NavBar.TRENDS.name,
+        component: Trending,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
-})
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { left: 0, top: 0 };
+  },
+});
 
-// Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
-    } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
-    }
-  } else {
-    console.error(err)
-  }
-})
-
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
-})
-
-export default router
+export default router;
