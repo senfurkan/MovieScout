ef0e3ae013c2fd7eb56a049d94a27f8b2b7f3c96
Furkan Şen <furkansen202@gmail.com>
2024-09-30 20:25:30 +0300

    [appConstants.ts]:[created]

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,26 @@
+export const HTTP_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+  };
+  
+  export const NavBar = {
+    HOME_VIEW: {
+      path: "/home",
+      name: "Anasayfa",
+    },
+    /* SEARCH_FOR_A_MOVIE: {
+      path: "/search-for-a-movie",
+      name: "Film Ara",
+    }, */
+    TRENDS: {
+      path: "/trends",
+      name: "Trendler",
+    },
+  };
+  
