58695a4aed0ff8f383123887cb8dcb33260661f8
Furkan Şen <furkansen202@gmail.com>
2024-09-30 20:24:59 +0300

    [Default.vue]:[created]

---
+++ src/layouts/Default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <nav-bar />
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import NavBar from "@/components/Navbar.vue";
+</script>
