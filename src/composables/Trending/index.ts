ad2f1519df21c3e620861bb82a68b865cfd0c1a9
Furkan Şen <furkansen202@gmail.com>
2024-10-26 00:06:24 +0300

    [index.ts]:[updated] A cleaner structure was created instead of repetitive codes

--- src/composables/Trending/index.ts
+++ src/composables/Trending/index.ts
@@ -1,6 +1,7 @@
 import { ref } from "vue";
 import { trendingAll, trendingMovie, trendingTV, trendingPerson } from "@/api";
 import { Trending } from "@/types/Trending/Index";
+import { Person } from "@/types/Trending/person";
 
 export function useTrending() {
   const filtering = ref("day");
@@ -16,67 +17,34 @@ export function useTrending() {
     { text: "Tümü", value: "all", icon: "mdi-expand-all-outline" },
     { text: "Filmler", value: "movie", icon: "mdi-movie-open" },
     { text: "Diziler", value: "tv", icon: "mdi-television-box" },
-    /* { text: "Oyuncular", value: "person", icon: "mdi-account-group" }, */
+    { text: "Oyuncular", value: "person", icon: "mdi-account-group" },
   ];
 
-  const lists = ref<Trending[]>([]);
+  /* const lists = tab.value != "person" ? ref<Trending[]>([]) : ref<Person[]>([]); */
+  const lists = ref<Trending[] | Person[]>([]);
   const isLoading = ref(false);
+  const apiMap: Record<string, (item: string) => Promise<any>> = {
+    all: trendingAll,
+    movie: trendingMovie,
+    tv: trendingTV,
+    person: trendingPerson,
+  };
+
   const trending = (item: string, item2: string) => {
     isLoading.value = true;
-    switch (item2) {
-      case "movie":
-        trendingMovie(item)
-          .then((response) => {
-            lists.value = response?.data?.results;
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => {
-            isLoading.value = false;
-            Promise.resolve();
-          });
-        break;
-      case "tv":
-        trendingTV(item)
-          .then((response) => {
-            lists.value = response?.data?.results;
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => {
-            isLoading.value = false;
-            Promise.resolve();
-          });
-        break;
-      case "person":
-        trendingPerson(item)
-          .then((response) => {
-            lists.value = response?.data?.results;
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => {
-            isLoading.value = false;
-            Promise.resolve();
-          });
-        break;
-      default:
-        trendingAll(item)
-          .then((response) => {
-            lists.value = response?.data?.results;
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => {
-            isLoading.value = false;
-            Promise.resolve();
-          });
-        break;
-    }
+    const fetchTreding = apiMap[item2] || apiMap.all;
+
+    fetchTreding(item)
+      .then((response) => {
+        lists.value = response?.data?.results;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        isLoading.value = false;
+        Promise.resolve();
+      });
   };
 
   return {
